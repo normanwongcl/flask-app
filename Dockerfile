6e580883504ae49a37afad66873cd70b4f1b134f
Norman Wong <nich19032@outlook.com>
2020-06-14 06:38:09 +1000

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# pull official base image
+FROM python:3.8.3-slim-buster
+
+# set work directory
+WORKDIR /usr/src/app
+
+# set environment variables
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+# install system dependencies
+RUN apt-get update && apt-get install -y netcat
+
+# install dependencies
+RUN pip3 install --upgrade pip
+COPY ./requirements.txt /usr/src/app/requirements.txt
+COPY /requirements/ /usr/src/app/requirements/
+RUN pip3 install -r requirements/dev.txt
+
+# copy project
+COPY . /usr/src/app/
+
+# run entrypoint.sh
+ENTRYPOINT ["/usr/src/app/entrypoint.sh"]
---
+++ project/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+# Pull in config to init
+app.config.from_object("project.config.Config")
+# Create a new SQLAlchemy instance
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, email):
+        self.email = email
+
+
+@app.route("/")
+def hello_world():
+    return jsonify(hello="world")
+
+
+@app.route("/static/<path:filename>")
+def staticfiles(filename):
+    return send_from_directory(app.config["STATIC_FOLDER"], filename)
